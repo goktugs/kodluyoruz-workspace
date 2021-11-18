ee79e6bd5ed5208658ae0d0201a2f53969dc50e2
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2021-11-18 23:44:21 +0300

    Some parts are deleted due to changes

--- react/React HW-1/src/app.js
+++
@@ -1,3 +0,0 @@
-import getData from './lib/service.js';
-
-getData(1).then((data) => console.log(data));
--- react/index.js
+++
@@ -1,7 +0,0 @@
-import { hello, topla, cıkar, user, text } from './my-module.js';
-
-console.log(topla(2, 4));
-console.log(cıkar(5, 7));
-console.log(text);
-console.log(user);
-hello('mehmet');
